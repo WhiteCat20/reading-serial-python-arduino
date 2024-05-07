78cbe0ceaa057ed22020e56e5c307730cef8d072
Faiz Rahmadani <fzrahmadan@gmail.com>
2024-05-07 17:33:39 +0700

    adding save-csv-reading file, so after you run the code it will print all the outputs from the arduino, and save all of it in csv file

--- reading-serial.py
+++ reading-serial.py
@@ -1,9 +1,8 @@
-# %%
+
 import serial
 import time
 import datetime
 
-# %%
 # Replace 'COM4' with the port your Arduino is connected to
 # Raspi Port ttyACM0
 arduino_port = 'COM4'
@@ -11,13 +10,11 @@ baud_rate = 9600
 # Open the serial connection
 ser = serial.Serial(arduino_port, baud_rate)
 
-# %%
 # Get current date and time
 now = datetime.datetime.now()
 # Format the date-time string as 'MMDDYYYY-HHMM'
 date_time_str = now.strftime("%d%m%Y-%H%M")
 
-# %%
 start_time = time.time()
 while (time.time() - start_time) <= 30:  # Run for 30 seconds
     try:
--- save-csv-reading.py
+++ save-csv-reading.py
@@ -1,22 +1,15 @@
-# %%
 import serial
 import csv
-import matplotlib.pyplot as plt
-import seaborn as sns
 import time
-import numpy as np
 import datetime
-import os
 
-# %%
 # Replace 'COM4' with the port your Arduino is connected to
 # Raspi Port ttyACM0
-arduino_port = 'COM4'
+arduino_port = 'COM11'
 baud_rate = 9600
 # Open the serial connection
 ser = serial.Serial(arduino_port, baud_rate)
 
-# %%
 # Create a CSV file to store the data
 # Get current date and time
 now = datetime.datetime.now()
@@ -25,10 +18,9 @@ date_time_str = now.strftime("%d%m%Y-%H%M")
 csv_file_path = f'{date_time_str}.csv'
 with open(csv_file_path, 'w', newline='') as csv_file:
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['sensor_value'])  # Header row
     
     start_time = time.time()
-    while (time.time() - start_time) <= 30:  # Run for 60 seconds
+    while (time.time() - start_time) <= 7200:  # Run for 60 seconds
         try:
             # Read a line from the serial port
             line = ser.readline().decode().strip()
@@ -45,7 +37,6 @@ ser.close()
 print("Serial connection closed.")
 
 
-# %%
 # Read data from CSV file
 data = []
 with open(csv_file_path, 'r') as csv_file:
@@ -54,36 +45,5 @@ with open(csv_file_path, 'r') as csv_file:
     for row in csv_reader:
         data.append(float(row[0]))
 
-# %%
-# Calculate vmin and vmax from the data
-vmin = min( )
-vmax = 4
-
-# %%
-# Assuming 'heatmaps' folder exists, if not, create it
-if not os.path.exists('heatmaps'):
-    os.makedirs('heatmaps')
-# Get the dynamic shape based on the size of the data
-heatmap_shape = (1, len(data))
-# Reshape data for heatmap
-heatmap_data = np.array(data).reshape(*heatmap_shape)
-
-# Create heatmap
-plt.figure(figsize=(12, 8))
-# heatmap themes = viridis, magma
-sns.heatmap(heatmap_data, annot=False, fmt=".5f", cmap="magma", xticklabels=10, yticklabels=10, cbar=False,
-             vmin=vmin, vmax=vmax
-)
-plt.title('')
-plt.xlabel('')
-plt.ylabel('')
-plt.xticks([])
-plt.yticks([])
-plt.axis('off')
-
-# Save the heatmap in the 'heatmaps' folder
-plt.savefig(f'heatmaps/{date_time_str}.png', bbox_inches='tight')
-
-plt.show()
 
 
